1b682cee6233c3dd987aeeabde396046ad9b3456
Melike Maranki <melikemaranki@gmail.com>
2020-11-02 15:18:56 +0300

    Wikidata Python query for bands whose name starts with R is written.

---
+++ wikidata.py
@@ -0,0 +1,31 @@
+# pip install sparqlwrapper
+# https://rdflib.github.io/sparqlwrapper/
+
+import sys
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+endpoint_url = "https://query.wikidata.org/sparql"
+
+query = """#Rock bands that start with "R"
+SELECT DISTINCT ?band ?bandLabel
+WHERE
+{
+  ?band  wdt:P31 wd:Q5741069 .
+        ?band rdfs:label ?bandLabel .
+  FILTER(STRSTARTS(?bandLabel, 'R')) .
+}"""
+
+
+def get_results(endpoint_url, query):
+    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    # TODO adjust user agent; see https://w.wiki/CX6
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+
+results = get_results(endpoint_url, query)
+
+for result in results["results"]["bindings"]:
+    print(result)
