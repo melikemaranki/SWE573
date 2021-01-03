8362f78342b9e3c3c309c950858252ecfa37a965
Melike Maranki <melikemaranki@gmail.com>
2021-01-03 20:47:54 +0300

    Search and barplot features are added.

--- SWE573/urls.py
+++ SWE573/urls.py
@@ -23,6 +23,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', dashboard_views.profile, name = 'profile'),
+    path('showResults/', dashboard_views.showResults, name = 'showResults'),
     path('register/', user_views.register, name = 'register'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
