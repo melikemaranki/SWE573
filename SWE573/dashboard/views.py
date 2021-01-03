08497a46f6ca0731076b71610bd183b6326e9b63
Melike Maranki <melikemaranki@gmail.com>
2021-01-03 20:47:54 +0300

    Search and barplot features are added.

--- SWE573/dashboard/views.py
+++ SWE573/dashboard/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render 
 from django.contrib.auth.decorators import login_required
+from .getData import getData
+from .utils import get_plot
 
 @login_required
 def profile(request):
     return render(request, 'dashboard/profile.html')
+
+
+def showResults(request):
+    if request.method == 'GET':
+        # Create a form instance and populate it with data from the request (binding):
+        text = request.GET['search']
+        data = getData(text)
+        chart =  get_plot(data)
+        return render(request, 'dashboard/showResults.html', {"search": text, "chart":chart})
