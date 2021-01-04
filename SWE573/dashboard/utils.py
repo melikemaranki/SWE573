b8554fc051ff7252677e2034aebba4d0b60065f4
Melike Maranki <melikemaranki@gmail.com>
2021-01-04 23:18:58 +0300

    Wordcloud is shown in the 2nd tab of the web app.

--- SWE573/dashboard/utils.py
+++ SWE573/dashboard/utils.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
 import itertools, collections
-
+from wordcloud import WordCloud, STOPWORDS
+import urllib
 
 def get_graph():
     buffer = BytesIO()
@@ -24,5 +25,16 @@ def get_plot(all_words):
     plt.figure(figsize=(10,10))
     plt.xticks(rotation =  45)
     plt.bar(x,y)
+    graph = get_graph()
+    return graph
+
+def get_wordcloud(all_words):
+    text = " ".join(all_words)
+    # Generate wordcloud
+    wc = WordCloud(width = 3000, height = 2000, random_state=1, background_color='black', colormap='Set2', collocations=False, stopwords = STOPWORDS).generate(text)
+    
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+
     graph = get_graph()
     return graph
