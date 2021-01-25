867bf7f8c77a794a3b14322af51fbd8f5dc87f2c
Melike Maranki <melikemaranki@gmail.com>
2021-01-26 01:58:14 +0300

    Querying last 5 searches feature is added.

--- SWE573/SWE573/urls.py
+++ SWE573/SWE573/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('profile/', dashboard_views.profile, name = 'profile'),
     path('fetchData/', dashboard_views.fetchData, name = 'fetchData'),
     path('showChart/', dashboard_views.showChart, name = 'showChart'),
+    path('showChart_with_ID/', dashboard_views.showChart_with_ID, name = 'showChart_with_ID'),
     path('register/', user_views.register, name = 'register'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
--- SWE573/dashboard/views.py
+++ SWE573/dashboard/views.py
@@ -10,17 +10,23 @@ import time
 
 @login_required
 def profile(request):
-    return render(request, 'dashboard/profile.html')
+    q_res = MyView(request)
+    return render(request, 'dashboard/profile.html', {'q_res': q_res})
 
+def MyView(request):
+    #Getting the last 5 searches of the requesting user
+    query_results = Tweets.objects.filter(user=request.user).distinct("search_id").order_by("-search_id")[:5]
+    return query_results
+    #return a response to your template and add query_results to the context
 
 def fetchData(request):
     if request.method == 'GET':
         # Create a form instance and populate it with data from the request (binding):
         text = request.GET['search']
         s_id = getData(request = request, text = text)
-        
+        q_res = MyView(request)
         #s_id = Tweets.objects.filter(user=request.user).latest("search_id").search_id
-        return render(request, 'dashboard/showResults.html', {"search": text,  "search_id":s_id})
+    return render(request, 'dashboard/showResults.html', {"search": text,  "search_id":s_id, "q_res":q_res})
 
 def showChart(request):
     s_id = Tweets.objects.filter(user=request.user).latest("search_id").search_id
@@ -29,4 +35,18 @@ def showChart(request):
     data =  clean_stopwords(df)
     chart = get_plot(data)
     wcloud = get_wordcloud(data)
-    return render(request, 'dashboard/showResults.html', {"chart": chart, "wcloud":wcloud,  "s_test":s_id, "count": tweet_count})
+    q_res = MyView(request)
+    return render(request, 'dashboard/showResults.html', {"chart": chart, "wcloud":wcloud,  "s_test":s_id, "count": tweet_count, 'q_res': q_res})
+
+def showChart_with_ID(request):
+    if request.method == 'GET':
+        s_id = request.GET['id']
+    else:
+        print("fail")
+    tweet_count = Tweets.objects.filter(search_id=s_id).count()
+    df = DataFrame.from_dict(Tweets.objects.filter(search_id = s_id).values("tweet_text"))
+    data =  clean_stopwords(df)
+    chart = get_plot(data)
+    wcloud = get_wordcloud(data)
+    q_res = MyView(request)
+    return render(request, 'dashboard/showResults.html', {"chart": chart, "wcloud":wcloud,  "s_test":s_id, "count": tweet_count, 'q_res': q_res})
